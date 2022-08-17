2df0e60365dac274f589506e22f06755b5c4a048
Anubhav Rajput <anubhavrajput193@gmail.com>
2022-08-18 00:17:16 +0530

    reverse first k elements

--- queue/ReverseFirstKElements.java
+++ queue/ReverseFirstKElements.java
@@ -1,10 +1,40 @@
 package queue;
-
+import java.util.*;
 public class ReverseFirstKElements {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		
+			Queue<Integer> q = new ArrayDeque<>();
+			q.add(1);
+			q.add(2);
+			q.add(3);
+			q.add(4);
+			q.add(5);
+			q.add(6);
+		    int k = 4;
+			
+			System.out.println(q);
+			reverseK(q, k);
+		    System.out.println(q);
+		}
+		static Queue<Integer> reverseK(Queue<Integer> q, int k) {
+			if(q.isEmpty() || k <= 0 || k > q.size()) return q;
+			int n = q.size();
+			ArrayDeque<Integer> stack = new ArrayDeque<>();
+			for(int i = 0; i<k; i++) {
+				stack.push(q.poll());
+			}
+			
+			while(!stack.isEmpty()) {
+				q.add(stack.pop());
+			}
+			
+			for(int i = 0; i<n-k; i++) {
+				q.add(q.poll());
+			}
+			
+			return q;
+		}
+	
 
 }
